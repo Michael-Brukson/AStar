f905a53beda626565a6e483a8c28f7ac7bbd4b0f
Michael <michael.brukson@my.liu.edu>
2026-02-16 14:11:04 -0500

    added centroid function, added set_available function

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ class Plotting():
     def __init__(self):
         pass
     
-    def show_grid(self, grid: list, path: list = None) -> None:
+    def show_grid(self, grid: list[int], path: list[tuple[int, int]] = []) -> None:
         plt.imshow(grid, cmap='gray')
 
         if np.any(path, None):
@@ -22,23 +22,46 @@ class Plotting():
         
         plt.show()
 
-    # TODO: Replace finding of sources with finding of centroids
-    def get_centroid():
-        pass
+    # return a tuple of ints representing a single coordinate that is the centroid of a groupd of coords.
+    def _centroid(self, coords: np.ndarray) -> tuple[int, int]:
+        # get all x and y coordinates
+        xs = coords[:, 1]
+        ys = coords[:, 0]
+
+        # simple average
+        x: int = int(np.mean(xs))
+        y: int = int(np.mean(ys))
+
+        return (x, y)
+
+    # in a black/white image with normalized values 0/1, set a list of coordinates to white (1)
+    def _set_available(self, img: np.ndarray, coords: np.ndarray) -> np.ndarray:
+        for coord in coords:
+            img[coord[0], coord[1]] = 1
+
+        return img
 
 
     def from_image(self, file: str) -> tuple:
-        img: np.ndarray = cv.imread(file)[:,:,:3]
-        # TODO: Replace this with finding centroids instead
-        # TODO: Once centroid found, replace rest of srcs/dests with white
-        src: tuple = np.argwhere(np.all(img == self.RED, axis=-1))[0]
-        dest: tuple = np.argwhere(np.all(img == self.BLUE, axis=-1))[0]
+        img: np.ndarray = cv.imread(file)[:,:,:3] # type: ignore
+
+        # get all pixels that represent sources and destinations
+        srcs: np.ndarray = np.argwhere(np.all(img == self.RED, axis=-1))
+        dests: np.ndarray = np.argwhere(np.all(img == self.BLUE, axis=-1))
+
+        # get centroid for src and destination
+        src = self._centroid(srcs)
+        dest = self._centroid(dests)
         
+        # convert to black/white 0/255
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        _, img = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+        # convert to black/white 0/1
         img = np.where(img == 255, 1, 0).astype(np.uint8)
 
-        img[src[0], src[1]] = 1
-        img[dest[0], dest[1]] = 1
+        # set source and destination to available
+        img = self._set_available(img=img, coords=srcs)
+        img = self._set_available(img=img, coords=dests)
 
         return (img, src, dest)
 
@@ -46,12 +69,10 @@ class Plotting():
 if __name__ == "__main__":
     p: Plotting = Plotting()
 
-    file: str = 'maze.png'
+    file: str = 'mazes/testing.png'
 
     grid, src, dest = p.from_image(file=file)
     
     print(f"{src} -> {dest}")
 
     p.show_grid(grid)
-
-    print(grid)
